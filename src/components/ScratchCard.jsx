695e017ffd7ea36558b45f8252f83c1f2d52292e
lovely2623 <mtthakur.786.mt@gmail.com>
2026-05-07 21:20:16 +0530

    fintech

--- src/components/ScratchCard.jsx
+++ src/components/ScratchCard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './SpinWheel.css';
+import './ScratchCard.css';
 
 const ScratchCard = () => {
     const [mobile, setMobile] = useState("");
